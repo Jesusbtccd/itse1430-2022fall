642a13c4119f8185039e9e6cd15e3c30bc61629c
Jesus Bustillos <jesus.bustillos@my.tccd.edu>
2022-08-29 21:05:39 -0500

    started movie library project

---
+++ classwork/Section1/MovieLibrary.ConsoleHost/Program.cs
@@ -0,0 +1,19 @@
+﻿//Movie definition
+string title = "";
+string description = "";
+int runLength = 0; //in minutes
+int releaseYear = 1900;
+string rating = "";
+bool isClassic = false;
+
+// string title = "";
+Console.WriteLine(" Enter a title : ");
+
+//strimng description = "";
+Console.WriteLine("Enter an optional description: ");
+
+//int runlength = 0; //in minutes
+Console.WriteLine("Enter a run lenrht (in minutees): ");
+Console.WriteLine("Enter the release year: ");
+Console.WriteLine("Enter MPAA rating: ");
+Console.WriteLine("Is this a classic? ");
--- classwork/Section1/Section1/Program.cs
+++ classwork/Section1/Section1/Program.cs
@@ -11,3 +11,29 @@ value = Console.ReadLine();
 double payRate = Double.Parse(value);
 
 Console.WriteLine("Your pay is " + (hours * payRate));
+
+
+
+int x;
+double distanceFromMoon = 0;
+//distanceFromMoon = 0;
+char letterGrade;
+bool isPassing;
+
+string name;
+string firstName = "Bob", lastName = "Smith";
+
+firstName = lastName = "name";
+//firstName = "Sam";
+//lastName = "Sam";
+
+//string lastName;
+
+//Block statement
+{
+    decimal payRate;
+}
+
+//distanceFromMoon = 10.5;
+isPassing = distanceFromMoon > 10;
+
