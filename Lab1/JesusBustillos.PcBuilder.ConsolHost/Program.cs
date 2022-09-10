d9d0c3bb7f17cab3be70f4758486599f91fa5ba0
Jesus Bustillos <jesus.bustillos@my.tccd.edu>
2022-09-10 15:56:31 -0500

    current changes

--- Lab1/JesusBustillos.PcBuilder.ConsolHost/Program.cs
+++ Lab1/JesusBustillos.PcBuilder.ConsolHost/Program.cs
@@ -2,3 +2,22 @@
 //ITSE-1430.Fall
 //08/3/2022
 
+Console.WriteLine("Jesus Bustillos");
+Console.WriteLine("ITSE 1430 Fall");
+Console.WriteLine("09/21/2022");
+
+string input = "";
+while (input != "Exit")
+{
+    Console.WriteLine();
+    Console.WriteLine("Choose an option: ");
+    Console.WriteLine("1: Processor");
+    Console.WriteLine("2: Memory");
+    Console.WriteLine("3: Exit");
+    //Console.WriteLine("Press Exit");
+    input = Console.ReadLine();
+    if (input == "1")
+    {
+
+    }
+};
